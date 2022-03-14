fe53321501c0eb4ab13c1fd84ab922825f9a70c6
Miracutor <miracleexecutors@gmail.com>
2022-03-14 15:04:01 +0800

    Add inView Hook

--- packages/miracutor-miracle-theme/src/Archive/index.tsx
+++ packages/miracutor-miracle-theme/src/Archive/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect, decode, loadable } from "frontity";
+import useInView from "@frontity/hooks/use-in-view";
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
@@ -11,15 +12,53 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import AuthorDateComponent from "../Post/AuthorDateComponent";
 import Pagination from "../Pagination";
 import { useCustomSsrMatchMedia } from "../utils";
+import Loading from "../Loading";
 
 const FeaturedMedia = loadable(() => import("../FeaturedMedia"));
+
+const PostArchive = (
+  item: any,
+  media: any,
+  post: any,
+  fetchMobileStatus: () => boolean,
+  author: any,
+  Html2React: any
+) => {
+  const { ref, inView } = useInView();
+
+  return (
+    <Box ref={ref} sx={{ mb: 5 }} key={item.id}>
+      {inView ? (
+        <Card variant="outlined" sx={{ py: 3, px: 1 }}>
+          {media && <CardMedia component={FeaturedMedia} media={media} />}
+          <CardContent>
+            <Link
+              underline="hover"
+              href={post.link}
+              sx={{ textAlign: "center" }}
+            >
+              <Typography variant={fetchMobileStatus() ? "h5" : "h4"}>
+                {decode(post.title.rendered.replace("&nbsp;", " "))}
+              </Typography>
+            </Link>
+            <AuthorDateComponent authorName={author.name} date={post.date} />
+            <Html2React html={post.content.rendered} />
+          </CardContent>
+        </Card>
+      ) : (
+        <Loading type={"POST"} />
+      )}
+    </Box>
+  );
+};
 
 const Archive = ({ state, actions, libraries, when }) => {
   const data = state.source.get(state.router.link);
   const Html2React = libraries.html2react.Component;
 
   const { ssrMatchMedia } = useCustomSsrMatchMedia(state.theme.userAgent);
-  const fetchMobileStatus = () => useMediaQuery("(max-width:768px)", { ssrMatchMedia });
+  const fetchMobileStatus = () =>
+    useMediaQuery("(max-width:768px)", { ssrMatchMedia });
 
   return (
     <Container sx={{ px: 0 }}>
@@ -28,28 +67,13 @@ const Archive = ({ state, actions, libraries, when }) => {
         const media = state.source.attachment[post.featured_media];
         const author = state.source.author[post.author];
 
-        return (
-          <Box sx={{ mb: 5 }} key={item.id}>
-            <Card variant="outlined" sx={{ py: 3, px: 1 }}>
-              {media && <CardMedia component={FeaturedMedia} media={media} />}
-              <CardContent>
-                <Link
-                  underline="hover"
-                  href={post.link}
-                  sx={{ textAlign: "center" }}
-                >
-                  <Typography variant={fetchMobileStatus() ? "h5" : "h4"}>
-                    {decode(post.title.rendered.replace("&nbsp;", " "))}
-                  </Typography>
-                </Link>
-                <AuthorDateComponent
-                  authorName={author.name}
-                  date={post.date}
-                />
-                <Html2React html={post.content.rendered} />
-              </CardContent>
-            </Card>
-          </Box>
+        return PostArchive(
+          item,
+          media,
+          post,
+          fetchMobileStatus,
+          author,
+          Html2React
         );
       })}
       <Pagination />
