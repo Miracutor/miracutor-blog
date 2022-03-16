1a971bd4ac591aa9a6398ade60f99cf774e44a2e
Miracutor <miracleexecutors@gmail.com>
2022-03-16 13:42:16 +0800

    Add Brotli compression

---
+++ packages/miracutor-miracle-theme/frontity.config.js
@@ -0,0 +1,22 @@
+const zlib = require("zlib");
+const CompressionPlugin = require("compression-webpack-plugin");
+
+export const webpack = ({ config, mode }) => {
+  if (mode === "production") {
+    config.plugins.push(
+      new CompressionPlugin({
+        filename: "[path][base].br",
+        algorithm: "brotliCompress",
+        test: /\.(js|css|html|svg)$/,
+        compressionOptions: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        },
+        threshold: 10240,
+        minRatio: 0.8,
+        deleteOriginalAssets: false,
+      })
+    );
+  }
+};
