9b9bf3fca98a40a00b903c8507568a6cad55e16e
Miracutor <miracleexecutors@gmail.com>
2022-03-26 11:12:37 +0800

    Remove Preact support

--- packages/miracutor-miracle-theme/frontity.config.js
+++
@@ -1,6 +0,0 @@
-export const webpack = ({ config }) => {
-    config.resolve.alias["react"] = "preact/compat";
-    config.resolve.alias["react-dom/test-utils"] = "preact/test-utils";
-    config.resolve.alias["react-dom"] = "preact/compat";
-    config.resolve.alias["react/jsx-runtime"] = "preact/jsx-runtime";
-  };
